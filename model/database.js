cf6b88e3b48e779940c2c0098946cdae8f4a3d7e
Audu Moses <89123661+MoscoDev@users.noreply.github.com>
2021-10-29 22:48:05 +0100

    Create database.js

---
+++ model/database.js
@@ -0,0 +1,18 @@
+exports.users =[
+    {
+      "firstName" :"john",
+      "lastName" :"Deee",
+      "id" : "1001" 
+    },
+        {
+      "firstName" :"johnny",
+      "lastName" :"bravo",
+      "id" : "1002" 
+    },
+        {
+      "firstName" :"john",
+      "lastName" :"Doe",
+      "id" : "1003" 
+    }
+  ];
+
